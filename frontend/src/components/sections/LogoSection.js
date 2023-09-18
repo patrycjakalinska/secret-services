00dd0c1fa4fac08d8438bc9424ffdb9b802fdff4
Patrycja Kalinska <cysiak0412@gmail.com>
2023-09-18 16:14:06 +0200

    add navbar and base model for one page scroll

---
+++ frontend/src/components/sections/LogoSection.js
@@ -0,0 +1,20 @@
+import '../../styles.css'
+
+const LogoSection = () => {
+  return (
+    <div className="Section">
+      <div className="LogoSection">
+        <div className="LogoSection__Text">
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LogoSection
---
+++ frontend/src/components/sections/WelcomeSection.js
@@ -0,0 +1,28 @@
+import '../../styles.css'
+
+const WelcomeSection = () => {
+  return (
+    <div>
+      <div className="WelcomeSection">
+        <div className="WelcomeSection__Icon">
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </div>
+        <div className="WelcomeSection__Text">
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomeSection
