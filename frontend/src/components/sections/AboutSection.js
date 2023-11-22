d3e1d24fd44721d26eb7d076cc4c4cc04118354e
Werostr <werost@student.agh.edu.pl>
2023-11-22 13:24:17 +0100

    add 'about' and 'welcome' section description

--- frontend/src/components/sections/AboutSection.js
+++ frontend/src/components/sections/AboutSection.js
@@ -1,95 +1,92 @@
-import React from 'react'
-import { Container } from '@mui/system'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import logo from '../../assets/access.png'
-import '../../styles.css'
+import React from "react";
+import { Container } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import logo from "../../assets/access.png";
+import "../../styles.css";
 
 const AboutSection = () => {
   return (
-    <div id='about'>
+    <div id="about">
       <Container
-        maxWidth='xl'
+        maxWidth="xl"
         sx={{
-          display: 'flex',
-          flexDirection: { lg: 'row', md: 'row', sm: 'column', xs: 'column' },
-          justifyContent: { lg: 'space-between', md: 'center' },
-          alignItems: 'center',
-          height: '100vh',
-          padding: '10rem',
-          paddingX: { lg: '5rem', md: '4rem', xs: '1rem' },
-          fontFamily: 'Inter',
+          display: "flex",
+          flexDirection: { lg: "row", md: "row", sm: "column", xs: "column" },
+          justifyContent: { lg: "space-between", md: "center" },
+          alignItems: "center",
+          height: "100vh",
+          padding: "10rem",
+          paddingX: { lg: "5rem", md: "4rem", xs: "1rem" },
+          fontFamily: "Inter",
         }}
       >
         <Container
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-            textAlign: 'left',
-            backgroundColor: '#3C404A',
-            borderRadius: '0 100px 0 100px ',
-            color: '#F1F0F0',
-            width: { lg: '60%', md: '60%', sm: '85%', xs: '85%' },
-            height: '30rem',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            textAlign: "left",
+            backgroundColor: "#3C404A",
+            borderRadius: "0 100px 0 100px ",
+            color: "#F1F0F0",
+            width: { lg: "60%", md: "60%", sm: "85%", xs: "85%" },
+            height: "30rem",
           }}
         >
           <Container
             sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
             }}
           >
             <Typography
-              variant='h2'
+              variant="h2"
               sx={{
-                fontFamily: 'Playfair Display',
-                fontSize: '38px',
-                textAlign: 'center',
-                marginBottom: '2rem',
+                fontFamily: "Playfair Display",
+                fontSize: "38px",
+                textAlign: "center",
+                marginBottom: "2rem",
               }}
             >
               About us
             </Typography>
             <Typography
-              variant='body1'
+              variant="body1"
               sx={{
-                paddingRight: { lg: '2rem', md: '1rem', sm: '1rem' },
-                lineHeight: 'normal',
-                letterSpacing: 'normal',
-                fontSize: { lg: '18px', md: '16px', sm: '16px', xs: '14px' },
+                paddingRight: { lg: "2rem", md: "1rem", sm: "1rem" },
+                lineHeight: "normal",
+                letterSpacing: "normal",
+                fontSize: { lg: "18px", md: "16px", sm: "16px", xs: "14px" },
               }}
             >
               <p>
-                Ut enim ad minima veniam, quis nostrum{' '}
-                <strong style={{ color: '#EC6D62' }}>
-                  exercitationem ullam{' '}
-                </strong>
-                corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-                consequatur?
-              </p>{' '}
+                We are with us since{" "}
+                <strong style={{ color: "#EC6D62" }}>2005</strong> and still
+                priding ourselves on ensuring professionalism and
+                confidentiality in every case we handle. We work from Monday to
+                Friday from 8 am to 8 pm. If You want to contact us write an
+                e-mail to mrDetective@secret.com or call us at +48 881234233.
+              </p>{" "}
               <p>
-                <strong>Voluptatem.</strong>
-              </p>
-              <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium
+                Let us be Your reliable partner in uncovering the information
+                You need —— Secret, where discretion meets results.
               </p>
             </Typography>
           </Container>
         </Container>
         <Box
           sx={{
-            display: { md: 'flex', sm: 'none', xs: 'none' },
-            marginLeft: '4rem',
+            display: { md: "flex", sm: "none", xs: "none" },
+            marginLeft: "4rem",
           }}
         >
-          <img src={logo} alt='logo' className='Logo' />
+          <img src={logo} alt="logo" className="Logo" />
         </Box>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- frontend/src/components/sections/WelcomeSection.js
+++ frontend/src/components/sections/WelcomeSection.js
@@ -1,104 +1,106 @@
-import '../../styles.css'
-import { Container } from '@mui/system'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
-import logo from '../../assets/lady.png'
+import "../../styles.css";
+import { Container } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import logo from "../../assets/lady.png";
 
 const WelcomeSection = () => {
   return (
-    <div id='blog'>
+    <div id="blog">
       <Container
-        maxWidth='xl'
+        maxWidth="xl"
         sx={{
-          display: 'flex',
-          flexDirection: { lg: 'row', md: 'row', sm: 'column', xs: 'column' },
-          justifyContent: { lg: 'space-between', md: 'center' },
-          alignItems: 'center',
-          height: '100vh',
-          padding: '10rem',
-          paddingX: { lg: '5rem', md: '4rem', xs: '1rem' },
-          fontFamily: 'Inter',
+          display: "flex",
+          flexDirection: { lg: "row", md: "row", sm: "column", xs: "column" },
+          justifyContent: { lg: "space-between", md: "center" },
+          alignItems: "center",
+          height: "100vh",
+          padding: "10rem",
+          paddingX: { lg: "5rem", md: "4rem", xs: "1rem" },
+          fontFamily: "Inter",
         }}
       >
-        <Box sx={{ display: { md: 'flex', sm: 'none', xs: 'none' } }}>
-          <img src={logo} alt='logo' className='Logo' />
+        <Box sx={{ display: { md: "flex", sm: "none", xs: "none" } }}>
+          <img src={logo} alt="logo" className="Logo" />
         </Box>
         <Container
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-            textAlign: 'left',
-            backgroundColor: '#3C404A',
-            borderRadius: '100px 0 100px 0',
-            color: '#F1F0F0',
-            width: '85%',
-            height: '30rem',
-            marginLeft: { lg: '8rem', md: '5rem', sm: '2.5rem', xs: '0.8rem' },
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            textAlign: "left",
+            backgroundColor: "#3C404A",
+            borderRadius: "100px 0 100px 0",
+            color: "#F1F0F0",
+            width: "85%",
+            height: "30rem",
+            marginLeft: { lg: "8rem", md: "5rem", sm: "2.5rem", xs: "0.8rem" },
           }}
         >
           <Container>
             <Typography
-              variant='h2'
+              variant="h2"
               sx={{
-                fontFamily: 'Playfair Display',
+                fontFamily: "Playfair Display",
                 paddingRight: {
-                  lg: '2rem',
-                  md: '1.8rem',
-                  sm: '1.6rem',
-                  xs: '1rem',
+                  lg: "2rem",
+                  md: "1.8rem",
+                  sm: "1.6rem",
+                  xs: "1rem",
                 },
                 marginBottom: {
-                  lg: '1.2rem',
-                  md: '1rem',
-                  sm: '3rem',
-                  xs: '1rem',
+                  lg: "1.2rem",
+                  md: "1rem",
+                  sm: "3rem",
+                  xs: "1rem",
                 },
-                fontSize: { lg: '50px', md: '45px', sm: '48px', xs: '36px' },
+                fontSize: { lg: "50px", md: "45px", sm: "48px", xs: "36px" },
               }}
             >
-              Lorem Ipsum <span style={{ color: '#EC6D62' }}>firten</span>, si.
+              Welcome to <span style={{ color: "#EC6D62" }}>Secret</span>.
             </Typography>
             <Typography
-              variant='body1'
+              variant="body1"
               sx={{
-                paddingRight: { lg: '8rem', md: '2.5rem', sm: '2rem' },
-                lineHeight: 'normal',
-                letterSpacing: 'normal',
-                fontSize: { lg: '18px', md: '16px', sm: '16px', xs: '14px' },
+                paddingRight: { lg: "8rem", md: "2.5rem", sm: "2rem" },
+                lineHeight: "normal",
+                letterSpacing: "normal",
+                fontSize: { lg: "18px", md: "16px", sm: "16px", xs: "14px" },
               }}
             >
-              Bottom text with quick description goes here. Or anything you
-              want. Description of services, of company and people.
+              We are a discreet and efficient online detective agency
+              specializing in surveillance and person location services. Our
+              agency operates in the background, dedicated to uncovering truths
+              and solving mysteries.
             </Typography>
           </Container>
-          <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Container sx={{ display: "flex", justifyContent: "center" }}>
             <Button
-              variant='contained'
+              variant="contained"
               disableElevantion
-              size='large'
+              size="large"
               sx={{
-                display: 'flex',
-                borderRadius: '30px',
+                display: "flex",
+                borderRadius: "30px",
                 fontWeight: 600,
-                fontSize: '18px',
+                fontSize: "18px",
                 marginTop: {
-                  lg: '2rem',
-                  md: '1.8rem',
-                  sm: '1.5rem',
-                  xs: '1.2rem',
+                  lg: "2rem",
+                  md: "1.8rem",
+                  sm: "1.5rem",
+                  xs: "1.2rem",
                 },
                 padding: {
-                  lg: '1rem 3rem',
-                  md: '0.5rem 2.4rem',
-                  sm: '0.6rem 1.6rem',
-                  xs: '0.5rem 1.5rem',
+                  lg: "1rem 3rem",
+                  md: "0.5rem 2.4rem",
+                  sm: "0.6rem 1.6rem",
+                  xs: "0.5rem 1.5rem",
                 },
-                color: '#FBF3F3',
-                backgroundColor: '#EC6D62',
-                textTransform: 'none',
-                '&:hover': { backgroundColor: '#FBF3F3', color: '#EC6D62' },
+                color: "#FBF3F3",
+                backgroundColor: "#EC6D62",
+                textTransform: "none",
+                "&:hover": { backgroundColor: "#FBF3F3", color: "#EC6D62" },
               }}
             >
               Spy now.
@@ -107,7 +109,7 @@ const WelcomeSection = () => {
         </Container>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default WelcomeSection
+export default WelcomeSection;
